db1a3adfd98d9843eae36e8ee7018c48822e320d
Kaisa1614 <35688012+Kaisa1614@users.noreply.github.com>
2018-02-08 21:19:46 +0200

    keilauksen pistellaskurin koodin parantelua

--- Pistelaskuri.cs
+++ Pistelaskuri.cs
@@ -8,15 +8,54 @@ using KeilauksenPistelaskuri;
 
 namespace KeilauksenPistelaskuri
 {
-
     public class Frame
     {
 
     }
 
- 
-    
-    public class TestienAlustus
+    public class Pistelaskuri
     {
+        private readonly Frame[] _frames = new Frame[10];
+
+        private int _currentFrame;
+
+        private Frame LastFrame { get { return _frames[_currentFrame - 1]; } }
+
+        private Frame TwoFramesAgo { get { return _frames[_currentFrame - 2]; } }
+
+        public int Score { get; private set; }
+
+        public void BowlFrame(Frame frame)
+        {
+            AddMarkBonuses(frame);
+
+            Score += frame.Total;
+            _frames[_currentFrame++] = frame;
+        }
+
+        private void AddMarkBonuses(Frame frame)
+        {
+            if (WereLastTwoFramesStrikes())
+                Score += frame.Total + frame.FirstThrow;
+            else if (WasLastFrameAStrike())
+                Score += frame.Total;
+            else if (WasLastFrameASpare())
+                Score += frame.FirstThrow;
+        }
+
+        private bool WereLastTwoFramesStrikes()
+        {
+            return WasLastFrameAStrike() && _currentFrame > 1 && TwoFramesAgo.IsStrike;
+        }
+
+        private bool WasLastFrameAStrike()
+        {
+            return _currentFrame > 0 && LastFrame.IsStrike;
+        }
+
+        private bool WasLastFrameASpare()
+        {
+            return _currentFrame > 0 && LastFrame.IsSpare;
+        }
     }
 }
